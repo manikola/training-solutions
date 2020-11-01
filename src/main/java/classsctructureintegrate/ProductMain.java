7a3a7ce0117b016acfd7e482d46d792c1e8a2430
manikola <66733574+manikola@users.noreply.github.com>
2020-11-01 15:33:56 +0100

    Classtructureintegrate commit

---
+++ src/main/java/classsctructureintegrate/ProductMain.java
@@ -0,0 +1,25 @@
+package classsctructureintegrate;
+
+import java.util.Scanner;
+
+public class ProductMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter product name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter price:");
+
+        int price = scanner.nextInt();
+
+        Product product = new Product(name, price);
+
+        System.out.println(product.getName() + " " + product.getPrice());
+        product.increasePrice(20);
+        System.out.println(product.getName() + " " + product.getPrice());
+        product.decreasePrice(10);
+        System.out.println(product.getName() + " " + product.getPrice());
+    }
+
+}
