6040ef1faf20d6dd44522d26cd46709af8c81232
manikola <66733574+manikola@users.noreply.github.com>
2020-11-10 10:49:19 +0100

    enumtype commit

--- src/main/java/enumtype/week/WorkdayCalculatorMain.java
+++ src/main/java/enumtype/week/WorkdayCalculatorMain.java
@@ -1,6 +1,6 @@
 package enumtype.week;
 
-public class WoerkdayCalculatorMain {
+public class WorkdayCalculatorMain {
     public static void main(String[] args) {
 
         System.out.println(new WorkdayCalculator().dayTypes(Day.SUNDAY, 8));
