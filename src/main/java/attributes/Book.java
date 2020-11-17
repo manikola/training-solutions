80d3c4add80612e193d413847c4dbb4912d425c8
manikola <66733574+manikola@users.noreply.github.com>
2020-11-17 11:29:44 +0100

    attributes commit

--- src/main/java/attributes/Book.java
+++ src/main/java/attributes/Book.java
@@ -5,14 +5,17 @@ public class Book {
     private String title;
 
     public Book(String title) {
+
         this.title = title;
     }
 
     public void setTitle(String title) {
+
         this.title = title;
     }
 
     public String getTitle() {
+
         return title;
     }
 
