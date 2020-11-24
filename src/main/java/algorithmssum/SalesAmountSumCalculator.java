c2f106fc53e6a68622968691ce9247eabea946ab
manikola <66733574+manikola@users.noreply.github.com>
2020-11-24 08:06:12 +0100

    algorithmssum commit

---
+++ src/main/java/algorithmssum/SalesAmountSumCalculator.java
@@ -0,0 +1,14 @@
+package algorithmssum;
+
+import java.util.List;
+
+public class SalesAmountSumCalculator {
+
+    public int sumSalesAmount(List<Salesperson> salespersons) {
+        int sum = 0;
+        for( Salesperson item : salespersons) {
+            sum += item.getAmount();
+        }
+        return sum;
+    }
+}
