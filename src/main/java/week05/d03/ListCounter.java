811b00f87ece5685a10f50a7dae917a1a85eb107
manikola <66733574+manikola@users.noreply.github.com>
2020-11-25 13:16:14 +0100

    ListCounter commit

---
+++ src/main/java/week05/d03/ListCounter.java
@@ -0,0 +1,36 @@
+package week05.d03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListCounter {
+
+
+        public int countAletters(List<String> words) {
+        int count = 0;
+        for (String item: words) {
+            String s = item.substring(0, 1).toLowerCase();
+            if (s.equals("a")) {
+                count++;
+            }
+        }
+
+            return count;
+        }
+    public static void main(String[] args) {
+        ListCounter listCounter = new ListCounter();
+        List<String> myList = new ArrayList<>();
+        myList.add("alma");
+        myList.add("banán");
+        myList.add("Aniko");
+        myList.add("körte");
+        System.out.println(""+listCounter.countAletters(myList));
+
+    }
+
+
+    }
+
+
