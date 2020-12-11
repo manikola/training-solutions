e6894fc2895f3a69b92c70543811fa351f51083b
manikola <66733574+manikola@users.noreply.github.com>
2020-12-11 09:41:20 +0100

    interfacedefaultmethods commit

---
+++ src/main/java/interfacedefaultmethods/print/ColoredPage.java
@@ -0,0 +1,24 @@
+package interfacedefaultmethods.print;
+
+public class ColoredPage {
+
+    private String page;
+    private String color;
+
+    public ColoredPage(String page, String color) {
+        this.page = page;
+        this.color = color;
+    }
+
+    public ColoredPage(String page) {
+        this.page = page;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
