213d88454a93e1ab0e920898066f0f836819864c
manikola <66733574+manikola@users.noreply.github.com>
2021-01-04 13:14:58 +0100

    week10d01 commit

---
+++ src/main/java/week10d01/Hiking.java
@@ -0,0 +1,36 @@
+package week10d01;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hiking {
+
+
+    public static double getPlusElevation(List<Double> height) {
+        double sum = 0.0;
+        for (int i = 1; i < height.size(); i++) {
+            if (height.get(i) > height.get(i-1)) {
+                sum += (height.get(i) - height.get(i-1));
+            }
+
+        }
+        return sum;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        List<Double> points = new ArrayList();
+        points.add(10.0);
+        points.add(20.0);
+        points.add(15.0);
+        points.add(18.0);
+
+        System.out.println(getPlusElevation(points));
+    }
+}
