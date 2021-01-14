9229d12b5833119e3df8460d4895d583f0dadecf
manikola <66733574+manikola@users.noreply.github.com>
2021-01-14 09:12:02 +0100

    exceptionmulticatch commit

---
+++ src/main/java/exceptionmulticatch/AnswerStat.java
@@ -0,0 +1,33 @@
+package exceptionmulticatch;
+
+
+
+public class AnswerStat {
+    //private static final Logger LOGGER = LoggerFactory.getLogger(AnswerStat.class);
+
+    private BinaryStringConverter converter;
+
+    public AnswerStat(BinaryStringConverter binaryStringConverter) {
+        this.converter = binaryStringConverter;
+    }
+
+    public int answerTruePercent(String answers) {
+        boolean[] booleans = convert(answers);
+        int numberOfTrue = 0;
+        for (int j = 0; j < booleans.length; j++) {
+            if (booleans[j]) {
+                numberOfTrue++;
+            }
+        }
+        return (int)Math.round((double)numberOfTrue / booleans.length * 100);
+    }
+
+    public boolean[] convert(String checkboxes) {
+        try {
+            return converter.binaryStringToBooleanArray(checkboxes);
+        } catch (NullPointerException | IllegalArgumentException ex) {
+            throw new InvalidBinaryStringException(ex);
+        }
+    }
+
+}
