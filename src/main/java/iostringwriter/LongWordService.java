b70c808a6754b93696a94d598f7cba76725fec7d
manikola <66733574+manikola@users.noreply.github.com>
2021-01-18 17:16:49 +0100

    iostringwriter commit

---
+++ src/main/java/iostringwriter/LongWordService.java
@@ -0,0 +1,28 @@
+package iostringwriter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+
+public class LongWordService {
+
+    public void writeLongWords(Writer writer, List<String> words) {
+        PrintWriter pw = new PrintWriter(writer);
+        for (String item : words) {
+            pw.print(item);
+            pw.print(":");
+            pw.println(item.length());
+        }
+    }
+
+    public String writeWithStringWriter(List<String> words) {
+        try (StringWriter sw = new StringWriter()) {
+            writeLongWords(sw, words);
+            return sw.toString();
+        } catch (IOException e) {
+            throw new IllegalStateException("Can not write");
+        }
+    }
+}
