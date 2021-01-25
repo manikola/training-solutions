e455686e7b1c45e7e45cc7eda2a7dc63e388777b
manikola <66733574+manikola@users.noreply.github.com>
2021-01-25 13:23:25 +0100

    week13d01 commit

---
+++ src/main/java/week13d01/Cities.java
@@ -0,0 +1,40 @@
+package week13d01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Cities {
+
+    public String longestCity(Path path) {
+        String longestCityName = null;
+        int max = 0;
+        path = Path.of("src/main/resources/iranyitoszamok-varosok-2021.csv");
+        try (BufferedReader br = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] cities = line.split(";");
+
+                String longestCity = null;
+                if (cities[1].length() > max) {
+                    max = cities[1].length();
+                    longestCityName = (cities[1]);
+                }
+
+            }
+        }catch(IOException e){
+                throw new IllegalStateException("Can not read file", e);
+            }
+            return longestCityName;
+        }
+
+
+
+
+
+    public static void main(String[] args) {
+        Cities c = new Cities();
+        System.out.println(c.longestCity());
+    }
+}
