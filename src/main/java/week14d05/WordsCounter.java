7cf325e260fc0b131b61625c6d42c1b228d7e228
manikola <66733574+manikola@users.noreply.github.com>
2021-02-05 13:14:18 +0100

    week14d05 commit

---
+++ src/main/java/week14d05/WordsCounter.java
@@ -0,0 +1,50 @@
+package week14d05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordsCounter {
+
+    public Map<String,Integer> countWords(Path path, String...words){
+        List<String> rows = listCreator(path);
+        Map<String,Integer> results= new HashMap<>();
+        for(String item :words){
+            int number =0;
+            for(String i :rows){
+                if(i.contains(item)){
+                    number++;
+                }
+            }
+            results.put(item,number);
+        }return results;
+
+       }
+
+    public List<String> listCreator(Path path){
+        path = Path.of("src/main/resources/hachiko.srt");
+        List<String> rows= new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(Path.of("src/main/resources/hachiko.srt"),Charset.forName("windows-1250"))) {
+            String line;
+            while((line = reader.readLine()) != null){
+                rows.add(line);
+
+            }return rows;
+
+        } catch (IOException e) {
+            throw new IllegalStateException("Can not read file",e);
+        }
+    }
+
+    public static void main(String[] args) {
+        WordsCounter wc = new WordsCounter();
+        Path path = Path.of("src/main/resources/hachiko.srt");
+        System.out.println(wc.countWords(path, "Hachiko", "haza", "pályaudvar", "jó"));
+    }
+}
